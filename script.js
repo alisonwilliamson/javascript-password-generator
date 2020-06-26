dde7c0c104e5433739b2da800e7cc398287a8563
Allie <williamson.alisonj@gmail.com>
2020-06-26 16:02:29 -0400

    updated HTML to include checkboxes for password criteria

---
+++ script.js
@@ -0,0 +1,18 @@
+// get password length
+let passwordLength = prompt("enter a length for the password");
+console.log(passwordLength);
+
+// get uppercase, lowercase, number, special
+
+// validate input - use conditional
+if (choicesMake){
+    generatePassword
+} else{
+    console.log("Please select some character")
+}
+
+
+// generate password
+//get the charactersets
+//randomly select charactersto make a password
+// display password
